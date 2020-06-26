a66adbc024338835fd74d735654924ea46a10c44
Sebastian Kent <sebastian.kent@thetrainline.com>
2020-06-26 21:23:20 +0100

    script issues init

--- build/dockerimage.sh
+++ build/dockerimage.sh
@@ -1,5 +1,6 @@
 echo login to docker
-IMAGE_NAME = sabkent/aspnet:$(date +%s)
+IMAGE_NAME=sabkent/aspnet:$(date +%s)
+echo image name is $IMAGE_NAME
 docker build --tag $IMAGE_NAME --file "PaymentGateway/Dockerfile"  . 
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 docker push $IMAGE_NAME
