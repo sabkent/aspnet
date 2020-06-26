a2f408553ca7aa322ac393960e78ae3d9920c74b
Sebastian Kent <sebastian.kent@thetrainline.com>
2020-06-26 21:37:22 +0100

    multi tags

--- build/dockerimage.sh
+++ build/dockerimage.sh
@@ -1,6 +1,6 @@
 echo login to docker
-IMAGE_NAME=sabkent/aspnet:$(date +%s)
+IMAGE_NAME=sabkent/aspnet
 echo image name is $IMAGE_NAME
-docker build --tag $IMAGE_NAME --file "PaymentGateway/Dockerfile"  . 
+docker build --tag $IMAGE_NAME:$(date +%s) --tag $IMAGE_NAME:latest --file "PaymentGateway/Dockerfile"  . 
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 docker push $IMAGE_NAME
