624d45748e24b0357f4201e5a5c152b5d0f77ccc
Sebastian Kent <sebastian.kent@thetrainline.com>
2020-06-26 21:11:14 +0100

    putting the github stuff into scripts

--- build/dockerimage.sh
+++ build/dockerimage.sh
@@ -1,1 +1,3 @@
-echo your docker username is $DOCKER_USERNAME
+echo login to docker
+docker build --tag sabkent/aspnet:$(date +%s) --file "PaymentGateway/Dockerfile"  . 
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
