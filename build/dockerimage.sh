c051dc47fa0f82871a21664c036725f46a7500f3
Sebastian Kent <sebastian.kent@thetrainline.com>
2020-06-26 21:15:30 +0100

    all da docking in one place

--- build/dockerimage.sh
+++ build/dockerimage.sh
@@ -1,3 +1,4 @@
 echo login to docker
 docker build --tag sabkent/aspnet:$(date +%s) --file "PaymentGateway/Dockerfile"  . 
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+docker push sabkent/aspnet:$(date +%s)
